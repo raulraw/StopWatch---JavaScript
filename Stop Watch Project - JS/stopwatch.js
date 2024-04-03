1424634fe6e8671bf016aeed0164fe3eed6a3113
Staicu Raul-Valentin <raul_valentin123@yahoo.com>
2024-04-03 18:48:50 +0300

    Add files via upload

--- Stop Watch Project - JS/stopwatch.js
+++ Stop Watch Project - JS/stopwatch.js
@@ -2,7 +2,7 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 let timeInterval;
-
+let watchIsActive =false;
 document.querySelector('.js-start-button').addEventListener('click', function() {
   startWatch();
 });
@@ -15,31 +15,72 @@ document.querySelector('.js-stop-button').addEventListener('click', function() {
   stopWatch();
 });
 
+// Get the modal
+var modal = document.getElementById("error-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
 function startWatch() {
-  timeInterval = setInterval(function() {
-    seconds++;
-    if (seconds >= 60) {
-      seconds = 0;
-      minutes++;
-      if (minutes >= 60) {
-        minutes = 0;
-        hours++;
+  if (!watchIsActive) {
+    watchIsActive = true;
+    timeInterval = setInterval(function() {
+      seconds++;
+      if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+          minutes = 0;
+          hours++;
+        }
       }
-    }
-    displayTime();
-  }, 1000);
+      displayTime();
+    }, 1000);
+  } else {
+    var errorMessage = document.getElementById("error-message");
+    errorMessage.innerText = "Fraiere, ceasul este deja pornit!";
+    modal.style.display = "block";
+  }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 function stopWatch() {
-  clearInterval(timeInterval);
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
-  displayTime();
+  if(seconds == 0 && minutes == 0 && hours == 0){
+    alert("Fraiere, nu vezi ca esti pe 0?");
+  }
+  else{
+    clearInterval(timeInterval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    displayTime();
+    watchIsActive = false;
+  }
+  
 }
 
 function pauseWatch() {
-  clearInterval(timeInterval);
+  if(watchIsActive == true){
+    clearInterval(timeInterval);
+    watchIsActive = false;
+  }
+  else
+  {
+    alert("Fraiere, ceasul e deja oprit!");
+  }
+  
 }
 
 function displayTime() {
